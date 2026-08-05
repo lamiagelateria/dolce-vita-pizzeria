6d182bf43512ce72ee1182ef4c21739348a63ca4
lamiagelateria <giuliamarto23@icloud.com>
2026-08-06 00:41:28 +0200

    Update pizze.js

--- pizze.js
+++ pizze.js
@@ -1,43 +1,61 @@
 const pizze = [
 
-{
-nome:"Margherita",
-prezzo:7
-},
+{nome:"Margherita", prezzo:7},
+{nome:"Marinara", prezzo:6},
+{nome:"Napoli", prezzo:8},
+{nome:"Romana", prezzo:8},
+{nome:"Diavola", prezzo:8},
+{nome:"Wurstel", prezzo:8},
+{nome:"Prosciutto", prezzo:8},
+{nome:"Prosciutto e funghi", prezzo:9},
+{nome:"Capricciosa", prezzo:10},
+{nome:"4 Stagioni", prezzo:10},
 
-{
-nome:"Marinara",
-prezzo:6
-},
+{nome:"4 Formaggi", prezzo:9},
+{nome:"Gorgonzola e noci", prezzo:11},
+{nome:"Bufalina", prezzo:10},
+{nome:"Burrata e pomodorini", prezzo:12},
+{nome:"Parmigiana", prezzo:10},
+{nome:"Vegetariana", prezzo:9},
+{nome:"Ortolana", prezzo:9},
+{nome:"Patate e rosmarino", prezzo:9},
+{nome:"Zucchine e ricotta", prezzo:10},
+{nome:"Melanzane grigliate", prezzo:10},
 
-{
-nome:"Diavola",
-prezzo:8
-},
+{nome:"Tonno e cipolla", prezzo:10},
+{nome:"Frutti di mare", prezzo:14},
+{nome:"Salmone e panna", prezzo:13},
+{nome:"Gamberetti", prezzo:13},
+{nome:"Mare e monti", prezzo:14},
 
-{
-nome:"4 Formaggi",
-prezzo:9
-},
+{nome:"Speck e brie", prezzo:11},
+{nome:"Bresaola rucola e grana", prezzo:12},
+{nome:"Crudo e mozzarella", prezzo:11},
+{nome:"Salame piccante e nduja", prezzo:11},
+{nome:"Carbonara", prezzo:11},
 
-{
-nome:"Capricciosa",
-prezzo:10
-},
+{nome:"Amatriciana", prezzo:10},
+{nome:"Cacio e pepe", prezzo:10},
+{nome:"Salsiccia e friarielli", prezzo:12},
+{nome:"Salsiccia e patate", prezzo:11},
+{nome:"Porcini", prezzo:12},
 
-{
-nome:"Bufalina",
-prezzo:10
-},
+{nome:"Tartufo", prezzo:15},
+{nome:"Tartufo e funghi", prezzo:16},
+{nome:"Dolce Vita Speciale", prezzo:14},
+{nome:"Pizza Chef", prezzo:13},
+{nome:"Pizza Gourmet", prezzo:15},
 
-{
-nome:"Prosciutto e funghi",
-prezzo:9
-},
+{nome:"Piccante speciale", prezzo:12},
+{nome:"Inferno", prezzo:12},
+{nome:"Messicana", prezzo:12},
+{nome:"BBQ", prezzo:13},
+{nome:"Americana", prezzo:11},
 
-{
-nome:"Vegetariana",
-prezzo:9
-}
+{nome:"Italia", prezzo:11},
+{nome:"Tricolore", prezzo:12},
+{nome:"Contadina", prezzo:10},
+{nome:"Montanara", prezzo:12},
+{nome:"Campagnola", prezzo:11}
 
 ];
