7ba0eb2cee71bf797b41b17f01ec3f9bb4afdcef
lamiagelateria <giuliamarto23@icloud.com>
2026-08-06 01:14:47 +0200

    Update prodotti.js

--- prodotti.js
+++ prodotti.js
@@ -1,21 +1,21 @@
 const prodotti = [
 
-{nome:"Patatine fritte", prezzo:4, categoria:"🍟 Fritti"},
+{nome:"🍟 Patatine fritte", prezzo:4, categoria:"Fritti"},
 
-{nome:"Acqua naturale", prezzo:2, categoria:"🥤 Bibite"},
-{nome:"Acqua frizzante", prezzo:2, categoria:"🥤 Bibite"},
-{nome:"Coca Cola", prezzo:3, categoria:"🥤 Bibite"},
-{nome:"Coca Cola Zero", prezzo:3, categoria:"🥤 Bibite"},
-{nome:"Fanta", prezzo:3, categoria:"🥤 Bibite"},
-{nome:"Sprite", prezzo:3, categoria:"🥤 Bibite"},
-{nome:"The freddo", prezzo:3, categoria:"🥤 Bibite"},
+{nome:"💧 Acqua naturale", prezzo:2, categoria:"Bibite"},
+{nome:"💧 Acqua frizzante", prezzo:2, categoria:"Bibite"},
+{nome:"🥤 Coca Cola", prezzo:3, categoria:"Bibite"},
+{nome:"🥤 Coca Cola Zero", prezzo:3, categoria:"Bibite"},
+{nome:"🥤 Fanta", prezzo:3, categoria:"Bibite"},
+{nome:"🥤 Sprite", prezzo:3, categoria:"Bibite"},
+{nome:"🥤 The freddo", prezzo:3, categoria:"Bibite"},
 
-{nome:"Birra Moretti", prezzo:4, categoria:"🍺 Birre"},
-{nome:"Birra Ichnusa", prezzo:4, categoria:"🍺 Birre"},
-{nome:"Heineken", prezzo:4, categoria:"🍺 Birre"},
-{nome:"Birra artigianale", prezzo:6, categoria:"🍺 Birre"},
+{nome:"🍺 Birra Moretti", prezzo:4, categoria:"Birre"},
+{nome:"🍺 Birra Ichnusa", prezzo:4, categoria:"Birre"},
+{nome:"🍺 Heineken", prezzo:4, categoria:"Birre"},
+{nome:"🍺 Birra artigianale", prezzo:6, categoria:"Birre"},
 
-{nome:"Vino rosso", prezzo:5, categoria:"🍷 Vini"},
-{nome:"Vino bianco", prezzo:5, categoria:"🍷 Vini"}
+{nome:"🍷 Vino rosso", prezzo:5, categoria:"Vini"},
+{nome:"🍷 Vino bianco", prezzo:5, categoria:"Vini"}
 
 ];
