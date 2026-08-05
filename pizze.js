c1377cc8a5792a126bf34f2ef71e6de8ae490d91
lamiagelateria <giuliamarto23@icloud.com>
2026-08-06 00:38:31 +0200

    Create pizze.js

---
+++ pizze.js
@@ -0,0 +1,43 @@
+const pizze = [
+
+{
+nome:"Margherita",
+prezzo:7
+},
+
+{
+nome:"Marinara",
+prezzo:6
+},
+
+{
+nome:"Diavola",
+prezzo:8
+},
+
+{
+nome:"4 Formaggi",
+prezzo:9
+},
+
+{
+nome:"Capricciosa",
+prezzo:10
+},
+
+{
+nome:"Bufalina",
+prezzo:10
+},
+
+{
+nome:"Prosciutto e funghi",
+prezzo:9
+},
+
+{
+nome:"Vegetariana",
+prezzo:9
+}
+
+];
