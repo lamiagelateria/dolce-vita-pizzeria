97d46ffe93ca5369e76092784aac45653dba9a62
lamiagelateria <giuliamarto23@icloud.com>
2026-08-06 01:06:25 +0200

    Create prodotti.js

---
+++ prodotti.js
@@ -0,0 +1,19 @@
+const prodotti = [
+
+{nome:"Patatine fritte", prezzo:4, categoria:"Fritti"},
+
+{nome:"Coca Cola", prezzo:3, categoria:"Bibite"},
+{nome:"Coca Cola Zero", prezzo:3, categoria:"Bibite"},
+{nome:"Fanta", prezzo:3, categoria:"Bibite"},
+{nome:"Sprite", prezzo:3, categoria:"Bibite"},
+{nome:"The freddo", prezzo:3, categoria:"Bibite"},
+
+{nome:"Birra Moretti", prezzo:4, categoria:"Birre"},
+{nome:"Birra Ichnusa", prezzo:4, categoria:"Birre"},
+{nome:"Heineken", prezzo:4, categoria:"Birre"},
+{nome:"Birra artigianale", prezzo:6, categoria:"Birre"},
+
+{nome:"Vino rosso", prezzo:5, categoria:"Vini"},
+{nome:"Vino bianco", prezzo:5, categoria:"Vini"}
+
+];
